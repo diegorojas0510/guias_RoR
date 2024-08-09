719b2496f100823e9c70e271e8e021b32c0b4e2e
Diego Rojas <diego.rojas@symplifica.com>
2024-08-08 19:22:13 -0500

    my first commit

---
+++ primeros_pasos.rb
@@ -0,0 +1,6 @@
+# irb
+# para ejecutar el código ruby la primera es abrir el interpretador de Ruby ejecutando el comando irb
+puts "Diferencia entre puts y print"
+
+puts "Hola Mundo"
+print "Hola Mundo\n"
